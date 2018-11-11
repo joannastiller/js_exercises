45cdd4e161e42291324b1f1380682ac5e36f0b7a
Joanna Stiller <joannastiller@yahoo.com>
2018-11-11 11:39:59 +0100

    Adds new elements to the array

---
+++ add to the array/main.js
@@ -0,0 +1,22 @@
+const names = [];
+const div = document.querySelector('div');
+
+const addName = (e) => {
+    e.preventDefault();
+    const input = document.querySelector('input');
+    const newName = input.value;
+    if (input.value.length) {
+        for (name of names) {
+            if (name === newName) {
+                alert('use a unique value');
+                input.value = '';
+                return
+            }
+        }
+        names.push(newName);
+        div.textContent += newName + ', ';
+        input.value = '';
+    }
+}
+
+document.querySelector('button').addEventListener('click', addName)
