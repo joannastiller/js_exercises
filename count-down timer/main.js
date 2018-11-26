5ddb6a32a58d8a9a96615cfdc9a01ce5c40ef92a
Joanna Stiller <joannastiller@yahoo.com>
2018-11-26 19:38:15 +0100

    Count-down timer

---
+++ count-down timer/main.js
@@ -0,0 +1,25 @@
+const endTime = new Date('2019-01-30 20:00:00').getTime();
+const spanD = document.querySelector('span.d');
+const spanH = document.querySelector('span.h');
+const spanM = document.querySelector('span.m');
+const spanS = document.querySelector('span.s');
+
+setInterval(() => {
+    const nowTime = new Date().getTime();
+    const time = endTime - nowTime;
+    const days = Math.floor(time / (1000 * 60 * 60 * 24));
+
+    let hours = Math.floor(time / (1000 * 60 * 60) % 24);
+    hours = hours < 10 ? `0${hours}` : hours;
+
+    let minutes = Math.floor(time / (1000 * 60) % 60);
+    minutes = minutes < 10 ? `0${minutes}` : minutes;
+
+    let seconds = Math.floor(time / 1000 % 60);
+    seconds = seconds < 10 ? `0${seconds}` : seconds;
+
+    spanD.textContent = days;
+    spanH.textContent = hours;
+    spanM.textContent = minutes;
+    spanS.textContent = seconds;
+}, 1000);
